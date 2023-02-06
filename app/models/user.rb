0b91b42081c05e05f98494a95a550ce810c24274
edgeston <m.edgeston130@gmail.com>
2023-02-05 20:03:55 -0600

    Create Sign Up Button

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@
 
 class User < ApplicationRecord
     has_secure_password
-
+    
     validates :email, presence:true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "Please use valid Email Address" } 
 end
